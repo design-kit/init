239b69269f47639c6ff806b3a622f0a8f3e8b032
kristof0425 <dombi.kristof@gmail.com>
2019-10-17 23:31:52 +0200

    feat(index): add prompt about name

--- src/index.js
+++ src/index.js
@@ -3,20 +3,21 @@ const inquirer = require('inquirer')
 const shell = require('shelljs')
 const ora = require('ora')
 const jsonfile = require('jsonfile')
-const { red, cyan, green, bold } = require('kleur')
+const { red, cyan, green, white, bold, italic } = require('kleur')
 let fs = require('fs')
 let path = require('path')
 let url = require('url')
 
 const frameworks = [
-  { title: 'React', value: 'react' },
-  { title: 'Vue', value: 'vue' },
-  { title: 'Svelte', value: 'svelte' },
+  { title: `React`, value: 'react' },
+  { title: `Vue ${white(italic('- coming soon'))}`, value: 'vue' },
+  { title: `Svelte ${white(italic('- coming soon'))}`, value: 'svelte' },
 ]
 
 ;(async () => {
   let appName
   let frameworkChoice
+
   const program = new commander.Command()
     .version('0.0.1')
     .option('-f, --framework [framework]', 'framework choice')
@@ -27,6 +28,20 @@ const frameworks = [
       frameworkChoice = options.framework
     })
     .allowUnknownOption()
+    .parse(process.argv)
+
+  if (!appName) {
+    const prompt = {
+      type: 'input',
+      name: 'appName',
+      message: 'You haven\'t provided the name of your project, what is it?',
+      pageSize: 4,
+    }
+
+    await inquirer.prompt(prompt).then(answers => {
+      appName = answers.appName
+    })
+  }
 
   if (!program.framework) {
     const output = []
@@ -51,5 +66,5 @@ const frameworks = [
     })
   }
 
-  console.log(`\n\nYou chose: ${frameworkChoice}\n\n`)
+  console.log(`\n\nName: ${appName}\n\nYou chose: ${frameworkChoice}\n\n`)
 })()
